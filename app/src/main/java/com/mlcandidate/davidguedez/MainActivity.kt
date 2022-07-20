4001185c608d586804e87f4c526110cac4eea3f0
David Guedez <david.guedez@frogmi.com>
2022-07-20 00:18:51 -0400

    Set features up and create basi ui for search products

--- app/src/main/java/com/mlcandidate/davidguedez/MainActivity.kt
+++ app/src/main/java/com/mlcandidate/davidguedez/MainActivity.kt
@@ -2,17 +2,37 @@ package com.mlcandidate.davidguedez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mlcandidate.davidguedez.ui.main.MainFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setUpNavigationController()
+        //goToSearchProduct()
     }
+
+    private fun setUpNavigationController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun goToSearchProduct() {
+        navController.navigate(R.id.action_mainFragment_to_foundProductsResult)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        val navigate = navController.navigateUp()
+        if (!navigate) {
+            finish()
+        }
+        return navigate
+
+    }
+
 }
