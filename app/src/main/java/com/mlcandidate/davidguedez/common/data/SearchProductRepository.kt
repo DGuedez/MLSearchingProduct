e76fbd9591754f553f4cb0e39494d726925ff32f
David Guedez <david.guedez@frogmi.com>
2022-07-24 00:58:36 -0400

    Remove unnecessary param for search repository function

--- app/src/main/java/com/mlcandidate/davidguedez/common/data/SearchProductRepository.kt
+++ app/src/main/java/com/mlcandidate/davidguedez/common/data/SearchProductRepository.kt
@@ -4,7 +4,6 @@ import com.mlcandidate.davidguedez.common.data.api.APIProductService
 import com.mlcandidate.davidguedez.common.data.api.model.mappers.ApiSearchMapper
 import com.mlcandidate.davidguedez.common.domain.model.NetworkException
 import com.mlcandidate.davidguedez.common.domain.model.product.Product
-import com.mlcandidate.davidguedez.common.domain.model.product.SearchProduct
 import com.mlcandidate.davidguedez.common.domain.repositories.ProductsRepository
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -13,9 +12,9 @@ class SearchProductRepository @Inject constructor(
     private val apiProductService: APIProductService,
     private val apiSearchMapper: ApiSearchMapper
 ) : ProductsRepository {
-    override suspend fun search(siteId: String,searchQuery: String): List<Product> {
+    override suspend fun search(searchQuery: String): List<Product> {
         try {
-            val searchResult = apiSearchMapper.mapToDomain(apiProductService.searchProduct(siteId,searchQuery))
+            val searchResult = apiSearchMapper.mapToDomain(apiProductService.searchProduct(query= searchQuery))
             return searchResult.results
         } catch (exception: HttpException) {
             throw NetworkException(exception.message() ?: "Code${exception.code()}")
--- app/src/main/java/com/mlcandidate/davidguedez/common/domain/repositories/ProductsRepository.kt
+++ app/src/main/java/com/mlcandidate/davidguedez/common/domain/repositories/ProductsRepository.kt
@@ -3,5 +3,5 @@ package com.mlcandidate.davidguedez.common.domain.repositories
 import com.mlcandidate.davidguedez.common.domain.model.product.Product
 
 interface ProductsRepository {
-    suspend fun search(siteId: String, searchQuery: String): List<Product>
+    suspend fun search(searchQuery: String): List<Product>
 }
