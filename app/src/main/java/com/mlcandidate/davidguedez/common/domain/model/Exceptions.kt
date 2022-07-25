b1a256f3dd6c871258bfcb7340cfe8dd5893d14e
David Guedez <david.guedez@frogmi.com>
2022-07-25 00:01:47 -0400

    Added network failure handle event implementation

--- app/src/main/java/com/mlcandidate/davidguedez/common/domain/model/Exceptions.kt
+++ app/src/main/java/com/mlcandidate/davidguedez/common/domain/model/Exceptions.kt
@@ -1,11 +1,9 @@
 package com.mlcandidate.davidguedez.common.domain.model
 
-import android.content.res.Resources
-import com.mlcandidate.davidguedez.R
 import java.io.IOException
 
 
 
-class NetworkUnavailableException(message: String = Resources.getSystem().getString(R.string.no_network_available)) : IOException(message)
+class NetworkUnavailableException(message: String = "No hay servicio de red disponible.") : IOException(message)
 
 class NetworkException(message: String): Exception(message)
